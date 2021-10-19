9a788219ee35833fbc008b87530516c0e2d9e1f8
Stefania Galazzo <galazzostefania@gmail.com>
2021-10-19 22:42:12 +0200

    carico file java

---
+++ JS/metodo2.js
@@ -0,0 +1,27 @@
+// METODO DUE
+let animal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda");
+
+switch(animal) {
+    case "1": 
+    console.log('Choose the Dog');
+    break;
+    
+    case "2": 
+    console.log('Choose the Cat');
+    break;
+
+    case "3": 
+    console.log('Choose the Goldfish');
+    break;
+
+    case "4": 
+    console.log('Choose the Monkey');
+    break;
+
+    case "5": 
+    console.log('Choose the Panda');
+    break;
+    
+    default: 
+    console.log('Animal not found');
+}
